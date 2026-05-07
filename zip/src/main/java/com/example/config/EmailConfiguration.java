e288983aaf6dfb07938c699284ed8415cd9a614b
Zackarya <148146350+zakaria12-3@users.noreply.github.com>
2026-05-08 00:25:01 +0100

    Update EmailConfiguration.java

--- zip/src/main/java/com/example/config/EmailConfiguration.java
+++ zip/src/main/java/com/example/config/EmailConfiguration.java
@@ -1,14 +1,10 @@
 package com.example.config;
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 import java.util.Properties;
-
 
 @Configuration
 public class EmailConfiguration {
@@ -17,30 +13,24 @@ public class EmailConfiguration {
     @Value("${spring.mail.password}")
     private String password;
 
-
     @Bean
     public JavaMailSender javaMailSender(){
-        JavaMailSenderImpl mailSender= new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp-relay.brevo.com");  
         mailSender.setPort(587);
         mailSender.setUsername(emailUsername);
         mailSender.setPassword(password);
 
-
-        Properties props= mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol","smtp");
-        props.put("mail.smtp.auth","true");
-        props.put("mail.smtp.starttls.enable","true");
-        props.put("mail.smtp.starttls.required","true");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");  
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", "smtp-relay.brevo.com");  
         props.put("mail.smtp.connectiontimeout", "5000");
         props.put("mail.smtp.timeout", "5000");
         props.put("mail.smtp.writetimeout", "5000");
-        props.put("mail.debug","false");
 
         return mailSender;
-
-
-
     }
 }
