58d8ad9dcfedf60e3354c50e475781d297a0b246
Zackarya <148146350+zakaria12-3@users.noreply.github.com>
2026-05-08 22:04:13 +0100

    Update SecurityConfiguration.java
    Added a new allowed origin for CORS configuration.

--- zip/src/main/java/com/example/config/SecurityConfiguration.java
+++ zip/src/main/java/com/example/config/SecurityConfiguration.java
@@ -69,7 +69,8 @@ public class SecurityConfiguration {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:4200",
-                "http://localhost:8027"
+                "http://localhost:8027",
+                "https://project-0ja8o.vercel.app" 
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
